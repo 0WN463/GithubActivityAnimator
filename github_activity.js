40d2a9855f34103856254ecd2cee2ba5dcb3331d
0WN463 <0WN463>
2021-07-07 14:08:45 +0800

    Extract FPS and BOX_PER_COL into constants

--- github_activity.js
+++ github_activity.js
@@ -1,14 +1,15 @@
-activity_boxes = [...document.getElementsByClassName('ContributionCalendar-day')]
+FPS = 30
+BOXES_PER_COL = 7
 
+activity_boxes = [...document.getElementsByClassName('ContributionCalendar-day')]
 activity_boxes.forEach(e => e.setAttribute("data-level", 0))
 
-frame = 0
-
-timer = setInterval(updateFrame, 1000/30)
-
 imageWidth = arr[0][0].length
-activityWidth = Math.floor(activity_boxes.length - 5)/7
-startIndex = Math.floor((activityWidth - imageWidth)/2) * 7
+activityWidth = Math.floor(activity_boxes.length - 5)/BOXES_PER_COL
+startIndex = Math.floor((activityWidth - imageWidth)/2) * BOXES_PER_COL
+
+frame = 0
+timer = setInterval(updateFrame, 1000/FPS)
 
 function updateFrame() {
 	if (frame >= arr.length) {
@@ -16,9 +17,9 @@ function updateFrame() {
 		return
 	}
 
-	for (let i = 0; i < 7; ++i)
+	for (let i = 0; i < BOXES_PER_COL; ++i)
 	    for (let j = 0; j < imageWidth; ++j)
-		activity_boxes[startIndex+j*7+i].setAttribute("data-level",arr[frame][i][j])
+		activity_boxes[startIndex+j*BOXES_PER_COL+i].setAttribute("data-level",arr[frame][i][j])
 
 	++frame
 }
