615392c6d57ea6cd13da83f05214004b3bca1b78
0WN463 <0WN463>
2021-07-07 14:40:46 +0800

    Rename github_activity.js to github_activity_animator.js. Rename variable from `arr` to `videoArr`.

--- github_activity_animator.js
+++ github_activity_animator.js
@@ -4,7 +4,7 @@ BOXES_PER_COL = 7
 activity_boxes = [...document.getElementsByClassName('ContributionCalendar-day')]
 activity_boxes.forEach(e => e.setAttribute("data-level", 0))
 
-imageWidth = arr[0][0].length
+imageWidth = videoArr[0][0].length
 activityWidth = Math.floor(activity_boxes.length - 5)/BOXES_PER_COL
 startIndex = Math.floor((activityWidth - imageWidth)/2) * BOXES_PER_COL
 
@@ -12,14 +12,14 @@ frame = 0
 timer = setInterval(updateFrame, 1000/FPS)
 
 function updateFrame() {
-	if (frame >= arr.length) {
+	if (frame >= videoArr.length) {
 		clearInterval(timer)
 		return
 	}
 
 	for (let i = 0; i < BOXES_PER_COL; ++i)
 	    for (let j = 0; j < imageWidth; ++j)
-		activity_boxes[startIndex+j*BOXES_PER_COL+i].setAttribute("data-level",arr[frame][i][j])
+		activity_boxes[startIndex+j*BOXES_PER_COL+i].setAttribute("data-level",videoArr[frame][i][j])
 
 	++frame
 }
